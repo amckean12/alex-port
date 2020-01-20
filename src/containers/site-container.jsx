7c0ff9db666de3e56993ae03f282f834f2df766c
amckean12 <amckean@me.com>
2020-01-20 16:24:48 -0600

    Creates Fronent architecture:

---
+++ src/containers/site-container.jsx
@@ -0,0 +1,15 @@
+//Libraries
+import React, { Component } from 'react';
+//Components
+
+
+class SiteContainer extends Component {
+    render() {
+        return (
+            <section className="hello-world">
+            </section>
+        )
+    }
+}
+
+export default SiteContainer
