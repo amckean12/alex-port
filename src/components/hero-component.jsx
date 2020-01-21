095936ca97d4a5316f8c49de58db246154231d40
amckean12 <amckean@me.com>
2020-01-20 19:22:03 -0600

    hero Work

--- src/components/hero-component.jsx
+++ src/components/hero-component.jsx
@@ -7,18 +7,14 @@ import TitleElement from './title-element.jsx'
 const HeroComponent = (props) => {
     return(
         <section className="hero">
-            <TitleElement
-                tag="h1" 
-                block="hero"
-                element="title"  
-                siteStyle={props.siteStyle}
-                content="Hello, I'm Alex, a Front End Developer." />
-            <TitleElement
-                tag="h2" 
-                block="hero"
-                element="sub-title"  
-                siteStyle={props.siteStyle}
-                content="How can I help?" />
+            <div className="hero__container">
+                <TitleElement
+                    tag="h1" 
+                    block="hero"
+                    element="title"  
+                    siteStyle={props.siteStyle}
+                    content="Building amazing sites in Kansas City." />
+            </div>
         </section>
     )
 }
